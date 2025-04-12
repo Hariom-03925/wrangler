25c0254a9e2733c2d7b150bdc805886937d88e4a
Hariom-03 <hariompatidar977@gmail.com>
2025-04-12 19:02:41 +0530

    update token type in TokenType.java

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
@@ -152,5 +152,8 @@ public enum TokenType implements Serializable {
    * Represents the enumerated type for the object of type {@code String} with restrictions
    * on characters that can be present in a string.
    */
-  IDENTIFIER
+  IDENTIFIER,
+  // newly added token type 
+  BYTE_SIZE,
+  TIME_DURATION
 }
